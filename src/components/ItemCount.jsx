f1abbbd026e3339f46a149b6b8aff1a68b99d5d2
Juan Pablo Ciceri <jpciceri@hotmail.com>
2023-05-28 19:44:42 -0300

    ultimos retoques

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -31,7 +31,7 @@ const ItemCount = ({stock}) => {
 
         <div className="container">
             <div className="row">
-                <div className="col">
+                <div className="col text-center">
                     <div className="btn-group" role="group" aria-label="Basic example">
                         <button type="button" className="btn btn-info" onClick={decrementarStock}>-</button>
                         <button type="button" className="btn btn-info">{items}</button>
@@ -40,7 +40,7 @@ const ItemCount = ({stock}) => {
                 </div>
             </div>
             <div className="row">
-                <div className="col">
+                <div className="col text-center my-1">
                     <button type="button" className="btn btn-info" onClick={onAdd}>Agregar al Carrito</button>
 
                 </div>
